7804f0d55d9a792991116819371119f941fadcb1
Jerome Le Saux <jeromelesaux@gmail.com>
2021-06-14 16:32:15 +0200

    add: add all widgets on ui.

--- ui/browser/browser.go
+++ ui/browser/browser.go
@@ -6,6 +6,7 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/jeromelesaux/impdos"
 )
@@ -20,9 +21,9 @@ func NewBrowser() *Browser {
 	return &Browser{}
 }
 
-func (b *Browser) Load(app fyne.App) {
+func (b *Browser) LoadDom(device string) {
 	var err error
-	b.imp, err = impdos.Read("/Users/jeromelesaux/Downloads/impdos_master_dump.img")
+	b.imp, err = impdos.Read(device)
 	if err != nil {
 		fmt.Printf("[LOADING] error :%v\n", err)
 	}
@@ -33,6 +34,11 @@ func (b *Browser) Load(app fyne.App) {
 	b.treeData = b.imp.GetTreePath()
 
 	b.treeView = widget.NewTreeWithStrings(b.treeData)
+	b.treeView.Refresh()
+}
+
+func (b *Browser) Load(app fyne.App) {
+	b.LoadDom("/Users/jeromelesaux/Downloads/impdos_master_dump.img")
 	b.treeView.OnSelected = func(id string) {
 		start := strings.Index(id, "(")
 		end := strings.LastIndex(id, ")")
@@ -47,11 +53,74 @@ func (b *Browser) Load(app fyne.App) {
 			fmt.Printf("Tree node unselected: %s", id)
 		}
 	*/
-	grid := container.NewGridWithColumns(1,
-		b.treeView)
+
+	// chemin du path du device
+	deviceForm := widget.NewEntry()
+	deviceForm.SetText("Device path:")
+	deviceForm.OnSubmitted = func(v string) {}
+	// bouton pour accéder au path du device
+	openDeviceButton := widget.NewButtonWithIcon("Open device", theme.FileIcon(), func() {
+		// here
+	})
+
+	deviceW := container.NewGridWithRows(2,
+		deviceForm,
+		openDeviceButton)
+
+	modeLabel := widget.NewLabel("Mode")
+	modeValue := widget.NewEntry()
+	modeContainer := container.NewGridWithColumns(2,
+		modeLabel, modeValue)
+
+	borderLabel := widget.NewLabel("Border")
+	borderValue := widget.NewEntry()
+	borderContainer := container.NewGridWithColumns(2,
+		borderLabel,
+		borderValue)
+
+	paperLabel := widget.NewLabel("Paper")
+	paperValue := widget.NewEntry()
+	paperContainer := container.NewGridWithColumns(2,
+		paperLabel,
+		paperValue)
+
+	inkLabel := widget.NewLabel("Ink")
+	inkValue := widget.NewEntry()
+	inkContainer := container.NewGridWithColumns(2,
+		inkLabel,
+		inkValue)
+
+	autoexecButton := widget.NewButton("Apply autoexec.", func() {})
+
+	autoexecContainer := container.NewGridWithRows(5,
+		modeContainer,
+		borderContainer,
+		paperContainer,
+		inkContainer,
+		autoexecButton)
+
+	backupButton := widget.NewButton("Backup your ImpDOS DOM", func() {})
+	restoreButton := widget.NewButton("Restore your ImpDOS DOM", func() {})
+	extractButton := widget.NewButton("Extract files or folder from you ImpDOS DOM", func() {})
+	importButton := widget.NewButton("Import your files or folder to your ImpDOS DOM", func() {})
+	domActionsContainer := container.NewGridWithRows(4,
+		backupButton,
+		restoreButton,
+		extractButton,
+		importButton)
+	cmdContainer := container.NewGridWithRows(6,
+		deviceW,
+		autoexecContainer,
+		domActionsContainer,
+	)
+
+	grid := container.NewGridWithColumns(2,
+		b.treeView,
+		cmdContainer)
+
 	win := app.NewWindow("IMPDos explorer")
 	win.SetContent(grid)
-	win.Resize(fyne.NewSize(1000, 800))
+	win.Resize(fyne.NewSize(1200, 800))
 	win.SetTitle("IMPDos explorer")
 	win.Show()
 }
