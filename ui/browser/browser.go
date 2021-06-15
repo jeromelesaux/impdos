efef59c0f7a7552beb13d2731f19841c91fc93ea
Jerome Le Saux <jeromelesaux@gmail.com>
2021-06-15 10:42:56 +0200

    add: add tree view update after loading dom.

--- ui/browser/browser.go
+++ ui/browser/browser.go
@@ -6,15 +6,40 @@ import (
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 	"github.com/jeromelesaux/impdos"
 )
 
 type Browser struct {
-	imp      *impdos.Impdos
-	treeData map[string][]string
-	treeView *widget.Tree
+	imp          *impdos.Impdos
+	treeData     map[string][]string
+	treeView     *widget.Tree
+	paper        *widget.Entry
+	border       *widget.Entry
+	ink          *widget.Entry
+	mode         *widget.Entry
+	devicePath   *widget.Entry
+	window       fyne.Window
+	treeViewGrid *fyne.Container
+}
+
+func (b *Browser) updateUi() {
+	if b.imp != nil {
+		v, err := b.imp.ReadAutoExec()
+		if err != nil {
+			fmt.Printf("[UPDATE UI AUTOEXEC] cannot update ui error : %v\n", err)
+			return
+		}
+
+		b.paper.SetText(fmt.Sprintf("%d", v.Paper))
+		b.border.SetText(fmt.Sprintf("%d", v.Border))
+		b.ink.SetText(fmt.Sprintf("%d", v.Ink))
+		b.mode.SetText(fmt.Sprintf("%d", v.Mode))
+
+	}
 }
 
 func NewBrowser() *Browser {
@@ -31,14 +56,25 @@ func (b *Browser) LoadDom(device string) {
 	if err != nil {
 		fmt.Printf("[LOADING] error :%v\n", err)
 	}
+	b.devicePath.SetText(device)
+	b.updateUi()
 	b.treeData = b.imp.GetTreePath()
-
-	b.treeView = widget.NewTreeWithStrings(b.treeData)
-	b.treeView.Refresh()
-}
-
-func (b *Browser) Load(app fyne.App) {
-	b.LoadDom("/Users/jeromelesaux/Downloads/impdos_master_dump.img")
+	b.treeView = widget.NewTree(
+		func(uid string) (c []string) {
+			c = b.treeData[uid]
+			return
+		},
+		func(uid string) (ok bool) {
+			_, ok = b.treeData[uid]
+			return
+		},
+		func(branch bool) fyne.CanvasObject {
+			return widget.NewLabel("Object")
+		},
+		func(uid string, branch bool, node fyne.CanvasObject) {
+			node.(*widget.Label).SetText(uid)
+		},
+	)
 	b.treeView.OnSelected = func(id string) {
 		start := strings.Index(id, "(")
 		end := strings.LastIndex(id, ")")
@@ -49,46 +85,78 @@ func (b *Browser) Load(app fyne.App) {
 		}
 		fmt.Printf("Tree node selected: %s with uuid :%s\n", id, uuid)
 	}
+	b.treeViewGrid.Objects[0] = b.treeView
+	b.treeView.Refresh()
+}
+
+func (b *Browser) Load(app fyne.App) {
+
 	/*	tree.OnUnselected = func(id string) {
 			fmt.Printf("Tree node unselected: %s", id)
 		}
 	*/
 
 	// chemin du path du device
-	deviceForm := widget.NewEntry()
-	deviceForm.SetText("Device path:")
-	deviceForm.OnSubmitted = func(v string) {}
+	b.devicePath = widget.NewEntry()
+	b.devicePath.SetText("Device path:")
+	b.devicePath.OnSubmitted = func(v string) {
+		var err error
+		b.imp, err = impdos.Read(v)
+		if err != nil {
+			fmt.Printf("[DEVICE LOADING] cannot load device error : %v\n", err)
+			return
+		}
+	}
 	// bouton pour accéder au path du device
 	openDeviceButton := widget.NewButtonWithIcon("Open device", theme.FileIcon(), func() {
+		fd := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
+			if err == nil && reader == nil {
+				return
+			}
+			if err != nil {
+				dialog.ShowError(err, b.window)
+				return
+			}
+			filename := reader.URI().Path()
+			b.LoadDom(filename)
+		}, b.window)
+		fd.SetFilter(storage.NewExtensionFileFilter([]string{".img", ".ibc"}))
+		fd.Resize(fyne.NewSize(1000, 800))
+		fd.Show()
 		// here
 	})
 
 	deviceW := container.NewGridWithRows(2,
-		deviceForm,
+		b.devicePath,
 		openDeviceButton)
 
 	modeLabel := widget.NewLabel("Mode")
-	modeValue := widget.NewEntry()
+	b.mode = widget.NewEntry()
 	modeContainer := container.NewGridWithColumns(2,
-		modeLabel, modeValue)
+		modeLabel,
+		b.mode,
+	)
 
 	borderLabel := widget.NewLabel("Border")
-	borderValue := widget.NewEntry()
+	b.border = widget.NewEntry()
 	borderContainer := container.NewGridWithColumns(2,
 		borderLabel,
-		borderValue)
+		b.border,
+	)
 
 	paperLabel := widget.NewLabel("Paper")
-	paperValue := widget.NewEntry()
+	b.paper = widget.NewEntry()
 	paperContainer := container.NewGridWithColumns(2,
 		paperLabel,
-		paperValue)
+		b.paper,
+	)
 
 	inkLabel := widget.NewLabel("Ink")
-	inkValue := widget.NewEntry()
+	b.ink = widget.NewEntry()
 	inkContainer := container.NewGridWithColumns(2,
 		inkLabel,
-		inkValue)
+		b.ink,
+	)
 
 	autoexecButton := widget.NewButton("Apply autoexec.", func() {})
 
@@ -99,28 +167,37 @@ func (b *Browser) Load(app fyne.App) {
 		inkContainer,
 		autoexecButton)
 
+	//b.LoadDom("/Users/jeromelesaux/Downloads/impdos_master_dump.img")
+	b.treeView = widget.NewTree(nil, nil, nil, nil)
+
 	backupButton := widget.NewButton("Backup your ImpDOS DOM", func() {})
 	restoreButton := widget.NewButton("Restore your ImpDOS DOM", func() {})
 	extractButton := widget.NewButton("Extract files or folder from you ImpDOS DOM", func() {})
 	importButton := widget.NewButton("Import your files or folder to your ImpDOS DOM", func() {})
-	domActionsContainer := container.NewGridWithRows(4,
+	deleteNode := widget.NewButton("Delete the selected file or folder", func() {})
+	createFolder := widget.NewButton("Create new folder", func() {})
+
+	domActionsContainer := container.NewGridWithRows(6,
 		backupButton,
 		restoreButton,
 		extractButton,
-		importButton)
-	cmdContainer := container.NewGridWithRows(6,
+		importButton,
+		deleteNode,
+		createFolder)
+
+	cmdContainer := container.NewGridWithRows(3,
 		deviceW,
 		autoexecContainer,
 		domActionsContainer,
 	)
 
-	grid := container.NewGridWithColumns(2,
+	b.treeViewGrid = container.NewGridWithColumns(2,
 		b.treeView,
 		cmdContainer)
 
-	win := app.NewWindow("IMPDos explorer")
-	win.SetContent(grid)
-	win.Resize(fyne.NewSize(1200, 800))
-	win.SetTitle("IMPDos explorer")
-	win.Show()
+	b.window = app.NewWindow("IMPDos explorer")
+	b.window.SetContent(b.treeViewGrid)
+	b.window.Resize(fyne.NewSize(1200, 800))
+	b.window.SetTitle("IMPDos explorer")
+	b.window.Show()
 }
