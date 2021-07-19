3ed6cd2b4e3a80655f5528435349b1fef72ef92a
Jerome Le Saux <jeromelesaux@gmail.com>
2021-07-19 17:17:37 +0200

    add: add exec command implinker.

---
+++ windows-linker.go
@@ -0,0 +1,62 @@
+package impdos
+
+import (
+	"bytes"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+)
+
+func getLinkerPath() (string, error) {
+	ex, err := os.Executable()
+	if err != nil {
+		return "", err
+	}
+	return filepath.Dir(ex) + "implinker.exe", nil
+}
+
+func readDomWin(startAddress, size int64) ([]byte, error) {
+	var b bytes.Buffer
+	exePath, err := getLinkerPath()
+	if err != nil {
+		return b.Bytes(), err
+	}
+
+	cmd := exec.Command(exePath, "read",
+		strconv.FormatInt(startAddress, 10),
+		strconv.FormatInt(size, 10))
+	cmd.Stdout = &b
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	return b.Bytes(), err
+}
+
+func writeDomWin(startAddress, size int64, buf []byte) error {
+	b := bytes.NewBuffer(buf)
+	exePath, err := getLinkerPath()
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command(exePath, "write",
+		strconv.FormatInt(startAddress, 10),
+		strconv.FormatInt(size, 10))
+	cmd.Stdin = b
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+	return err
+}
+
+func inquiringDomWin() (domSize, blockSize int64, err error) {
+	exePath, err := getLinkerPath()
+	if err != nil {
+		return
+	}
+	cmd := exec.Command(exePath, "inquiring")
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+	err = cmd.Run()
+
+	return
+}
